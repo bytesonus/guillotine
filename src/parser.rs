947a6bb9d94e7971a8e007f4047890d9385ef87f
Rakshith Ravi <rakshith.ravi@gmx.com>
2020-05-05 20:41:23 +0530

    Added fmt for stable rust

--- src/parser.rs
+++ src/parser.rs
@@ -62,8 +62,8 @@ async fn parse_if_config(input: &EnvRequirements) -> Result<bool> {
 	let mut satisfied = true;
 
 	if let Some(required_cfg) = &input.target_family {
-		if (required_cfg == "unix" && cfg!(target_family = "unix")) ||
-			(required_cfg == "windows" && cfg!(target_family = "windows"))
+		if (required_cfg == "unix" && cfg!(target_family = "unix"))
+			|| (required_cfg == "windows" && cfg!(target_family = "windows"))
 		{
 			satisfied &= true;
 		} else {
@@ -72,15 +72,15 @@ async fn parse_if_config(input: &EnvRequirements) -> Result<bool> {
 	}
 
 	if let Some(required_cfg) = &input.target_os {
-		if (required_cfg == "windows" && cfg!(target_os = "windows")) ||
-			(required_cfg == "macos" && cfg!(target_os = "macos")) ||
-			(required_cfg == "ios" && cfg!(target_os = "ios")) ||
-			(required_cfg == "linux" && cfg!(target_os = "linux")) ||
-			(required_cfg == "android" && cfg!(target_os = "android")) ||
-			(required_cfg == "freebsd" && cfg!(target_os = "freebsd")) ||
-			(required_cfg == "dragonfly" && cfg!(target_os = "dragonfly")) ||
-			(required_cfg == "openbsd" && cfg!(target_os = "openbsd")) ||
-			(required_cfg == "netbsd" && cfg!(target_os = "netbsd"))
+		if (required_cfg == "windows" && cfg!(target_os = "windows"))
+			|| (required_cfg == "macos" && cfg!(target_os = "macos"))
+			|| (required_cfg == "ios" && cfg!(target_os = "ios"))
+			|| (required_cfg == "linux" && cfg!(target_os = "linux"))
+			|| (required_cfg == "android" && cfg!(target_os = "android"))
+			|| (required_cfg == "freebsd" && cfg!(target_os = "freebsd"))
+			|| (required_cfg == "dragonfly" && cfg!(target_os = "dragonfly"))
+			|| (required_cfg == "openbsd" && cfg!(target_os = "openbsd"))
+			|| (required_cfg == "netbsd" && cfg!(target_os = "netbsd"))
 		{
 			satisfied &= true;
 		} else {
@@ -89,13 +89,13 @@ async fn parse_if_config(input: &EnvRequirements) -> Result<bool> {
 	}
 
 	if let Some(required_cfg) = &input.target_arch {
-		if (required_cfg == "x86" && cfg!(target_arch = "x86")) ||
-			(required_cfg == "x86_64" && cfg!(target_arch = "x86_64")) ||
-			(required_cfg == "mips" && cfg!(target_arch = "mips")) ||
-			(required_cfg == "powerpc" && cfg!(target_arch = "powerpc")) ||
-			(required_cfg == "powerpc64" && cfg!(target_arch = "powerpc64")) ||
-			(required_cfg == "arm" && cfg!(target_arch = "arm")) ||
-			(required_cfg == "aarch64" && cfg!(target_arch = "aarch64"))
+		if (required_cfg == "x86" && cfg!(target_arch = "x86"))
+			|| (required_cfg == "x86_64" && cfg!(target_arch = "x86_64"))
+			|| (required_cfg == "mips" && cfg!(target_arch = "mips"))
+			|| (required_cfg == "powerpc" && cfg!(target_arch = "powerpc"))
+			|| (required_cfg == "powerpc64" && cfg!(target_arch = "powerpc64"))
+			|| (required_cfg == "arm" && cfg!(target_arch = "arm"))
+			|| (required_cfg == "aarch64" && cfg!(target_arch = "aarch64"))
 		{
 			satisfied &= true;
 		} else {
@@ -104,8 +104,8 @@ async fn parse_if_config(input: &EnvRequirements) -> Result<bool> {
 	}
 
 	if let Some(required_cfg) = &input.target_endian {
-		if (required_cfg == "little" && cfg!(target_endian = "little")) ||
-			(required_cfg == "big" && cfg!(target_endian = "big"))
+		if (required_cfg == "little" && cfg!(target_endian = "little"))
+			|| (required_cfg == "big" && cfg!(target_endian = "big"))
 		{
 			satisfied &= true;
 		} else {
